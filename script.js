8cfeb37968cfb9ddfe69a7974db90642c30b6dea
Markandey Shukla <117027551+markandeyshukla@users.noreply.github.com>
2026-02-16 22:42:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let form=document.getElementById("MyForm")
 // submit.addEventListener("click",volume_sphere)
 function volume_sphere(e) {
 	e.preventDefault();
-    const pie=22/7;
+    const pie=Math.PI;
 	const n=4/3;
 	let rad=Number(r.value);
 	if(rad<0){
